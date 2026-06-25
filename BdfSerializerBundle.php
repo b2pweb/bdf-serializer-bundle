3db45960d6041c17e8ea0ab8121ca361628f56f3
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2026-06-25 15:55:27 +0200

    chore: Compatibility with SF 8 (#FRAM-228)

--- BdfSerializerBundle.php
+++ BdfSerializerBundle.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class BdfSerializerBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new SerializerNormalizerPass());
         $container->addCompilerPass(new SerializerLoaderPass());
--- DependencyInjection/Compiler/SerializerLoaderPass.php
+++ DependencyInjection/Compiler/SerializerLoaderPass.php
@@ -23,7 +23,7 @@ class SerializerLoaderPass implements CompilerPassInterface
         $this->loaderTag = $loaderTag;
     }
 
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
         if (!$container->hasDefinition($this->service)) {
             return;
